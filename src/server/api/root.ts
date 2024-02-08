1c27775f78ddf3af0eb3387ba256b906efd7277a
ahmad1702 <ahmad1702@gmail.com>
2024-02-07 18:44:16 -0600

    build fixes

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "@/server/api/routers/post";
 import { createTRPCRouter } from "@/server/api/trpc";
 import { petitionRouter } from "./routers/petition";
 
@@ -8,7 +7,6 @@ import { petitionRouter } from "./routers/petition";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   petition: petitionRouter,
 });
 
